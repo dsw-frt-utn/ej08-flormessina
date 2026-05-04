e412748f0e12b137f802325a986143373a62fa01
Messina Florencia <messinaflor05@gmail.com>
2026-05-04 14:01:08 -0300

    Problema 3: Agrego clase Product para manejo de referencias

---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,9 @@
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        public string Description { get; set; }
+        
+        public void SetDescription(string description) { Description = description; }
+    }
+}
