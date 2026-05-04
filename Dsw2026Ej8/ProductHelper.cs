91fde0da97b35ed27654ae7866d34da9d0273cba
Messina Florencia <messinaflor05@gmail.com>
2026-05-04 13:41:25 -0300

    Resuelto Problema 2: Implementación de clase anónima

--- Dsw2026Ej8/ProductHelper.cs
+++ Dsw2026Ej8/ProductHelper.cs
@@ -9,6 +9,18 @@ public string ObtenerEtiquetaProducto(long code, string description, decimal pri
             return $"[{code}] {description} -  {precioFinal}";
 
         }
+        
+        public object ObtenerInformacionProducto(long code, string description, decimal price)
+{
+    
+    return new 
+    {
+        Codigo = code,
+        Descripcion = description,
+        Precio = price,
+        FechaDeCarga = DateTime.Now // DateTime.Now te da la fecha y hora de tu PC
+    };
+}
 
 
     }
